0388170a30568cb2d6677cc7ab6181c1edf015dc
li316 <43591719+li316@users.noreply.github.com>
2019-11-11 09:03:16 +0800

    Add files via upload

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,22 +1,30 @@
 package cn.wzvtc.soft;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-//@CrossOrigin
-@RestController("/" )
+@CrossOrigin
+@RestController()
 public class TestController {
 
-    @RequestMapping(value="/data.json",method={RequestMethod.GET})
+    @RequestMapping(value="/data.json")
     public Map<String,String> bookById(){
         Map<String,String> resultMap=new HashMap<>();
         resultMap.put("myname","李家游");
         resultMap.put("mynumber","18002090216");
+        return resultMap;
+    }
+
+    @RequestMapping(value="/login")
+    public Map<String,String> login(@RequestBody Map<String,String> map) {
+        String password=map.get("password");
+        String number=map.get("number");
+        Map<String,String> resultMap=new HashMap<>();
+        if ("18002090216".equals(password) && "18002090216".equals(password)) {
+            resultMap.put("result", "success");
+        }
         return resultMap;
     }
 }
