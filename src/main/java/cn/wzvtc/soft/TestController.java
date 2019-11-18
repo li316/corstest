e1ef63ec6f549b11865e0ab7dc1de1c1fed0955a
li316 <43591719+li316@users.noreply.github.com>
2019-11-18 09:18:44 +0800

    Add files via upload

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -2,6 +2,7 @@ package cn.wzvtc.soft;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,20 +10,23 @@ import java.util.Map;
 @RestController()
 public class TestController {
 
-    @RequestMapping(value="/data.json")
-    public Map<String,String> bookById(){
+    @RequestMapping(value="/userinfo")
+    public Map<String,String> userinfo(HttpSession httpSession){
         Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","李家游");
-        resultMap.put("mynumber","18002090216");
+        if(httpSession.getAttribute("loginnumber")!=null) {
+            resultMap.put("myname", "李家游");
+            resultMap.put("mynumber", "18002090216");
+        }
         return resultMap;
     }
 
     @RequestMapping(value="/login")
-    public Map<String,String> login(@RequestBody Map<String,String> map) {
+    public Map<String,String> login(@RequestBody Map<String,String> map, HttpSession httpSession) {
         String password=map.get("password");
         String number=map.get("number");
         Map<String,String> resultMap=new HashMap<>();
         if ("18002090216".equals(password) && "18002090216".equals(password)) {
+            httpSession.setAttribute("loginnumber",number);
             resultMap.put("result", "success");
         }
         return resultMap;
